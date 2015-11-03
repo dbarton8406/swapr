4e847ef66be44ec2b45b942682d0a6a9d11e489c
Brit Butler <redline6561@gmail.com>
2015-11-03 10:41:01 -0500

    Require emails to have a . after the @ symbol.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates_uniqueness_of :email, :username
-  validates_format_of :email, with: /.+@.+/
+  validates_format_of :email, with: /.+@.+\..+/
 end
