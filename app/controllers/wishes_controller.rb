768883576de9997539a6cff183d80e56315d83fc
Brit Butler <redline6561@gmail.com>
2015-11-12 11:37:23 -0500

    Add json_defaults and refactor.

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -3,19 +3,19 @@ class WishesController < ApplicationController
 
   def show
     @wish = Wish.find(params[:id])
-    render 'show.json.jbuilder'
+    render :show, status: :ok
   end
 
   def index
     @page = params[:page] || 1
     @wishes = Wish.where(fulfilled: false).per(10).page(@page)
-    render 'index.json.jbuilder'
+    render :index, status: :ok
   end
 
   def create
     @wish = current_user.wishes.new(wish_params)
     if @wish.save
-      render 'create.json.jbuilder', status: :created
+      render :create, status: :created
     else
       render json: { errors: @wish.errors.full_messages },
         status: :unprocessable_entity
