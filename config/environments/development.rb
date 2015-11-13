744a7b3e7fbb00f6ff43ffbddc201db46beda87a
Brit Butler <redline6561@gmail.com>
2015-11-12 22:02:02 -0500

    Pull bucket info from environment.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
     s3_credentials: {
       access_key_id: ENV["S3_ACCESS_KEY"],
       secret_access_key: ENV["S3_SECRET_KEY"],
-      bucket: "swapr.tiy.test"
+      bucket: ENV["S3_TEST_BUCKET"]
     }
   }
 end
